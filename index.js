c96a695bc9f527639273ff5523da7dbbfc431733
grontho69 <mahathirm880@gmail.com>
2026-02-15 16:52:54 +0600

    eighth commit

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const express = require('express')
+
+
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 3000
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
+const admin = require("firebase-admin");
+
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
+
+
+
+// token verify
+
+
+const verifyFBToken = async (req,res,next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).send({ message:'unauthorize access'})
+  }
+  try {
+    const idToken = token.split(' ')[1]
+    const decoded = await admin.auth().verifyIdToken(idToken)
+    req.decoded_email = decoded.email
+    next()
+  }
+  catch (error) {
+    return res.status(401).send({ message:'unauthorize access'})
+  }
+}
+
+
+
+
 const uri = "mongodb+srv://assignment-11:cqH2wMuYMmT8G8WY@cluster0.pxios99.mongodb.net/?appName=Cluster0";
 app.use(cors())
 app.use(express.json())
@@ -11,10 +46,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// const serviceAccount = require("./firebase-admin-key.json");
 
-const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
-const serviceAccount = JSON.parse(decoded);
+
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -43,6 +81,12 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/user', verifyFBToken , async (req, res) => {
+      const result = await userCollections.find().toArray()
+      res.status(200).send(result)
+})
+
+
     app.get('/user/role/:email', async (req, res) => {
       const {email} = req.params
       const query = { email: email }
@@ -50,10 +94,29 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/update/user/status/:email', verifyFBToken, async (req, res) => {
+    
+    const email = req.params.email; 
+   
+    const status = req.query.status; 
+
+    const query = { email: email };
+    const updateStatus = {
+        $set: {
+            status: status 
+        }
+    };
+
+    const result = await userCollections.updateOne(query, updateStatus);
+    res.send(result);
+});
+
+   
+
 
     // request
     
-    app.post('/request', async (req,res) => {
+    app.post('/request',verifyFBToken, async (req,res) => {
       const data = req.body
       const result = await requestCollections.insertOne(data)
       res.send(result)
